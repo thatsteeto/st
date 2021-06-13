bcc283b07611a804b44e33e0238e979b487b4311
thatsteeto <augustoaltenhofen@protonmail.com>
2021-06-13 18:51:34 -0300

    more alpha configurations

--- config.h
+++ config.h
@@ -94,7 +94,9 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.90;
+float alpha = .8;
+float alphaOffset = 0.0;
+float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
